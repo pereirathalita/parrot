5e1737d98efffda5cde03f663062b36f8fae7b95
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-07 10:57:44 -0300

    feat:  routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import TelaCadastro from "./components/Cadastro";
 import Feed from "./components/Feed";
 import PerfilUsuario from "./components/Perfil";
-import RoutesApp from "./Routes";
+import RoutesApp from "../src/routes/Routes";
 
 export default function App() {
   return (
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import TelaCadastro from "./components/Cadastro";
-import Feed from "./components/Feed";
-import TelaLogin from "./components/Login";
-import PerfilUsuario from "./components/Perfil";
+import TelaCadastro from "../components/Cadastro";
+import Feed from "../components/Feed";
+import TelaLogin from "../components/Login";
+import PerfilUsuario from "../components/Perfil";
 
 export default function RoutesApp() {
   return (
