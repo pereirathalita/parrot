1a65e4993d2a05fd710d6c570856578901405e51
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-09-07 11:41:24 -0300

    feat: config redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,3 +3,7 @@ import { configureStore } from "@reduxjs/toolkit"
 const store = configureStore({
     reducer: {},
 });
+
+export type RootStore = ReturnType<typeof store.getState>;
+
+export default store;
